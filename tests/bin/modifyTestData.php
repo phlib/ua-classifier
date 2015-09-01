a66a1471a4b77704bcce87a0e719397ad66f4565
Chris Minett <chris@minett.me.uk>
2015-09-01 11:45:22 +0100

    Add script to allow mass-modification of the test case data

---
+++ tests/bin/modifyTestData.php
@@ -0,0 +1,71 @@
+#!/usr/bin/php
+<?php
+
+// Config
+
+$outputFile = dirname(__DIR__) . '/test-cases.yaml';
+
+$config = [
+    'device' => null, // regex, or null to skip; eg. /^iPad$/i
+    'os'     => null, // regex, or null to skip
+    'ua'     => null, // regex, or null to skip
+    'class'  => 'mobile',  // new class: desktop, tablet, mobile, spider
+    'test'   => true,      // display matching entries without modifying
+    'skipNonEmpty' => true // skip entries where class is already defined
+];
+
+// Script
+
+require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+if ($config['device'] === null && $config['os'] === null && $config['ua'] === null) {
+    throw new \RuntimeException('At least one match must be specified');
+}
+
+$testData = \Symfony\Component\Yaml\Yaml::parse($outputFile);
+
+$countTotal = 0;
+$countEmpty = 0;
+$countWritten = 0;
+foreach ($testData as $key => &$testCase) {
+    $countTotal++;
+    if (empty($testCase['class'])) {
+        $countEmpty++;
+    } elseif ($config['skipNonEmpty'] !== false) {
+        continue;
+    }
+
+    if ($config['device'] !== null && preg_match($config['device'], $testCase['device']) === 0) {
+        continue;
+    }
+    if ($config['os'] !== null && preg_match($config['os'], $testCase['os']) === 0) {
+        continue;
+    }
+    if ($config['ua'] !== null && preg_match($config['ua'], $testCase['ua']) === 0) {
+        continue;
+    }
+
+    // We're going to update this one
+    $countWritten++;
+    if (empty($testCase['class'])) {
+        $countEmpty--;
+    }
+
+    echo "Write '{$config['class']}' to key '{$key}' \n";
+
+    $testCase['class'] = $config['class'];
+}
+
+if ($config['test'] === false && $countWritten > 0) {
+    echo "\nWriting file...\n\n";
+    file_put_contents(
+        $outputFile,
+        \Symfony\Component\Yaml\Yaml::dump($testData, 1)
+    );
+}
+
+$mem = round(memory_get_usage() /1024);
+if ($config['test'] !== false) {
+    echo "Testing. No data changed\n";
+}
+echo "Complete. Written {$countWritten} entries, totalling {$countTotal}, leaving {$countEmpty} empty, using {$mem}kb memory\n\n";
