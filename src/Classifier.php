eedee87a87b0b9a595ec1570dc77e30d72dba006
Chris Minett <chris@minett.me.uk>
2015-09-18 15:00:42 +0100

    Move definitions out into class properties.

--- src/Classifier.php
+++ src/Classifier.php
@@ -9,34 +9,53 @@ namespace UAClassifier;
  */
 class Classifier
 {
+    /**
+     * @var array
+     */
+    public $mobileOSs = ['windows phone 6.5','windows ce','symbian os'];
+
+    /**
+     * @var array
+     */
+    public $mobileBrowsers = [
+        'firefox mobile','opera mobile','opera mini','mobile safari','webos','ie mobile','playstation portable',
+        'nokia','blackberry','palm','silk','android','maemo','obigo','netfront','avantgo','teleca','semc-browser',
+        'bolt','iris','up.browser','symphony','minimo','bunjaloo','jasmine','dolfin','polaris','brew','chrome mobile',
+        'uc browser','tizen browser'
+    ];
+
+    /**
+     * @var array
+     */
+    public $tablets = ['kindle','ipad','playbook','touchpad','dell streak','galaxy tab','xoom'];
+
+    /**
+     * @var array
+     */
+    public $mobileDevices = [
+        'iphone','ipod','ipad','htc','kindle','lumia','amoi','asus','bird','dell','docomo','huawei','i-mate','kyocera',
+        'lenovo','lg','kin','motorola','philips','samsung','softbank','palm','hp ','generic feature phone','generic smartphone'
+    ];
 
     /**
      * Get classification result for given Parser result
      *
      * @param \UAParser\Result\Client $parseResult
+     * @return Result
      */
     public function classify(\UAParser\Result\Client $parseResult)
     {
-        $mobileOSs      = array('windows phone 6.5','windows ce','symbian os');
-        $mobileBrowsers = array('firefox mobile','opera mobile','opera mini','mobile safari','webos','ie mobile','playstation portable',
-                                'nokia','blackberry','palm','silk','android','maemo','obigo','netfront','avantgo','teleca','semc-browser',
-                                'bolt','iris','up.browser','symphony','minimo','bunjaloo','jasmine','dolfin','polaris','brew','chrome mobile',
-                                'uc browser','tizen browser');
-        $tablets        = array('kindle','ipad','playbook','touchpad','dell streak','galaxy tab','xoom');
-        $mobileDevices  = array('iphone','ipod','ipad','htc','kindle','lumia','amoi','asus','bird','dell','docomo','huawei','i-mate','kyocera',
-                                'lenovo','lg','kin','motorola','philips','samsung','softbank','palm','hp ','generic feature phone','generic smartphone');
-
         $classifyResult = new Result();
 
         switch (true) {
-            case (in_array(strtolower($parseResult->device->family), $tablets)):
+            case (in_array(strtolower($parseResult->device->family), $this->tablets)):
                 $classifyResult->matchType      = 'device';
                 $classifyResult->isTablet       = true;
                 $classifyResult->isMobileDevice = true;
                 $classifyResult->isComputer     = false;
                 break;
 
-            case (in_array(strtolower($parseResult->device->family), $mobileDevices)):
+            case (in_array(strtolower($parseResult->device->family), $this->mobileDevices)):
                 $classifyResult->matchType      = 'device';
                 $classifyResult->isMobileDevice = true;
                 $classifyResult->isMobile       = true;
@@ -49,14 +68,14 @@ class Classifier
                 $classifyResult->isComputer = false;
                 break;
 
-            case (in_array(strtolower($parseResult->os->family), $mobileOSs)):
+            case (in_array(strtolower($parseResult->os->family), $this->mobileOSs)):
                 $classifyResult->matchType      = 'os';
                 $classifyResult->isMobileDevice = true;
                 $classifyResult->isMobile       = true;
                 $classifyResult->isComputer     = false;
                 break;
 
-            case (in_array(strtolower($parseResult->ua->family), $mobileBrowsers)):
+            case (in_array(strtolower($parseResult->ua->family), $this->mobileBrowsers)):
                 $classifyResult->matchType      = 'ua';
                 $classifyResult->isMobileDevice = true;
                 $classifyResult->isMobile       = true;
